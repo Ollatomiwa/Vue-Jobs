e37db01878d6fd5bbcd85f131233d1199db72b9e
ollatomiwa <ajaomoshood@gmail.com>
2024-10-17 21:09:32 +0100

    Add job page added and vue-toastification added to show notification if job is added successfully or not

--- joblisting/src/router/index.js
+++ joblisting/src/router/index.js
@@ -3,6 +3,7 @@ import Homeview from "@/views/Homeview.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,13 @@ const router = createRouter({
             path: '/jobs/:id',
             name: 'job',
             component: JobView,
+
+        },
+
+        {
+            path: '/jobs/add',
+            name: 'Add job',
+            component: AddJobView,
 
         },
 
